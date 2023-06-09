634841de50b94d69fa461b9235eb65c75bebce1d
vitaliyyuras <vitaliyyuras@gmail.com>
2023-06-09 18:03:53 +0300

    Added a null type check to solve the problem of type incompatibility

--- CryptoInfoViewer/Models/CryptoCurrency.cs
+++ CryptoInfoViewer/Models/CryptoCurrency.cs
@@ -15,21 +15,19 @@ namespace CryptoInfoViewer.Models
 {
     public class CryptoCurrency
     {
-
-
-
-        public string name { get; set; }
-        public string symbol { get; set; }
+        public string? id { get; set; }
+        public string? name { get; set; }
+        public string? symbol { get; set; }
 
         public int rank { get; set; }
-        public decimal supply { get; set; }
-        public decimal maxSupply { get; set; }
-        public decimal marketCapUsd { get; set; }
+        public decimal? supply { get; set; }
+        public decimal? maxSupply { get; set; }
+        public decimal? marketCapUsd { get; set; }
         
-        public decimal volumeUsd24Hr { get; set; }
-        public decimal priceUsd { get; set; }
-        public decimal changePercent24Hr { get; set; }
-        public decimal vwap24Hr { get; set; }
-        public string explorer { get; set; }
+        public decimal? volumeUsd24Hr { get; set; }
+        public decimal? priceUsd { get; set; }
+        public decimal? changePercent24Hr { get; set; }
+        public decimal? vwap24Hr { get; set; }
+        public string? explorer { get; set; }
     }
 }
